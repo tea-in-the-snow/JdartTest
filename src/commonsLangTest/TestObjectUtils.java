6bd10613d7bf5839360cab623e8ecde643c39c69
shaoran <xiashaoran@outlook.com.com>
2025-09-16 12:03:33 +0800

    add some small tests

--- src/commonsLangTest/TestObjectUtils.java
+++ src/commonsLangTest/TestObjectUtils.java
@@ -1,5 +1,7 @@
 package commonsLangTest;
 
+import org.apache.commons.lang3.ObjectUtils;
+
 public class TestObjectUtils {
     public static boolean test(int a) {
         if (a == 0) {
@@ -7,8 +9,19 @@ public class TestObjectUtils {
         }
         return true;
     }
+
+    public static Object testGetIfNull(Object obj, Object defaultObj) {
+        return ObjectUtils.getIfNull(obj, defaultObj);
+    }
+
+    public static Object testFirstNonNull(Object... objects) {
+        return ObjectUtils.firstNonNull(objects);
+    }
+
     public static void main(String[] args) {
-        test(0);
+        // testGetIfNull(null, new Integer("26"));
+        // test(0);
+        testFirstNonNull(null, null, "hello", "world");
         System.out.println("Testing ObjectUtils");
     }
 }
